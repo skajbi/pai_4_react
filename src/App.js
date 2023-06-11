79b9b4e7d79a92585396ca7663127b13a8a2dab8
student <student@ubuntu224-10.edu-wsb.local>
2023-06-11 11:47:02 +0200

    React app 1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  const subject = props.subject;
+  console.log(props);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello, {subject}!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
